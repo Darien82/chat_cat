e6d458329497a6bd57c0c0cc216d3f6debf09a52
Darien82 <DarienCurry@gmail.com>
2016-03-04 17:37:59 -0800

    fixing server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 'use strict';
 const express = require('express');
 const app = express();
+const ChatCAT = require('./app');
 
-app.set('port' , process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
-app.set('view engine' , 'ejs');
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-	res.render('login', {
-		pageTitle: 'My Login Page'
-	});
+app.get('/',(req, res, next) => {
+	res.render('login');
 });
 
 app.listen(app.get('port'), () => {
